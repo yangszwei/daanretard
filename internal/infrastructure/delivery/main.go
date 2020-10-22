4f7679b4879c997bb98e1ab070d1a458482c1db3
Szwei Yang <yangszwei@outlook.com>
2020-10-22 12:24:10 +0800

    Update infrastructure/delivery

--- internal/infrastructure/delivery/main.go
+++ internal/infrastructure/delivery/main.go
@@ -3,23 +3,34 @@ package delivery
 
 import (
 	"github.com/gin-gonic/gin"
+	"path"
 )
 
 // NewEngine create a Engine
 func NewEngine() *Engine {
 	e := new(Engine)
-	e.root = gin.Default()
+	e.engine = gin.Default()
+	e.root = e.engine.Group("/")
+	e.views = e.root
 	e.api = e.root.Group("/api")
 	return e
 }
 
 // Engine shortcuts to gin.Engine
 type Engine struct {
-	root *gin.Engine
+	engine *gin.Engine
+	root *gin.RouterGroup
+	views *gin.RouterGroup
 	api *gin.RouterGroup
+}
+
+// SetResourceRoot load html templates & add route for static assets
+func (e *Engine) SetResourceRoot(root string) {
+	e.engine.Static("/public", path.Join(root, "public"))
+	e.engine.LoadHTMLGlob(path.Join(root, "templates/**/*"))
 }
 
 // Run start engine
 func (e *Engine) Run(addr string) error {
-	return e.root.Run(addr)
+	return e.engine.Run(addr)
 }
